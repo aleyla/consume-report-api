bd447a081916b008743e5bb85c68714b888a6dea
aysegul.ilhan <aysegul.ilhan@emlakjet.com>
2021-11-27 13:07:17 +0300

    initial project

---
+++ src/main/java/com/aleyla/consumereportapi/ConsumeReportApiApplication.java
@@ -0,0 +1,13 @@
+package com.aleyla.consumereportapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumeReportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumeReportApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleyla/consumereportapi/ConsumeReportApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleyla.consumereportapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumeReportApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
