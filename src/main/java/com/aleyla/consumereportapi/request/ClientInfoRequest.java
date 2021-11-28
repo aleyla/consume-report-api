cce2578e7868de9df8766195435d4db8bd1866da
aysegul.ilhan <aysegul.ilhan@emlakjet.com>
2021-11-29 02:42:21 +0300

    Change lombok

--- src/main/java/com/aleyla/consumereportapi/request/ClientInfoRequest.java
+++ src/main/java/com/aleyla/consumereportapi/request/ClientInfoRequest.java
@@ -2,10 +2,12 @@ package com.aleyla.consumereportapi.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class ClientInfoRequest {
 
